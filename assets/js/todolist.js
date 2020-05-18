990c7c0b304e20979e450438e8ae24cc78bfc68d
mizanxali <mizanalip786@gmail.com>
2020-05-18 09:55:47 +0530

    Added add item functionality

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,12 +1,24 @@
 //check off todo by clicking
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("checked");
 });
 
 //delete todo
-$(".delete-btn").on("click", function(event) {
+$("ul").on("click", ".delete-btn", function(event) {
   $(this).parent().fadeOut(1000, function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+//add todo
+$("input[type='text']").keypress(function(event){
+  //check if enter key pressed inside text input
+  if(event.which===13) {
+    var todoText = $(this).val();
+    //clear input text
+    $(this).val("");
+    //add <li> to <ul>
+    $("ul").append("<li><span class='delete-btn'>X</span> " + todoText + "</li>");
+  }
 })
